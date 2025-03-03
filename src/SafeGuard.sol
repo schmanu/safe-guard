f6e83931d265236a7f8def11fa6517111c581e5a
schmanu <manu@safe.global>
2025-03-03 14:54:09 +0100

    feat: adds allowed transferTargets to configuratin and checks erc20 and native transfers
    - calls _checkTransaction recursively for multiSends

--- src/SafeGuard.sol
+++ src/SafeGuard.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.20;
 
 import {IERC165} from "./interfaces/IERC165.sol";
 import {IModuleGuard} from "./interfaces/IModuleGuard.sol";
-import {ISafe, MODULE_GUARD_STORAGE_SLOT, TRANSACTION_GUARD_STORAGE_SLOT} from "./interfaces/ISafe.sol";
+import {ISafe, TRANSACTION_GUARD_STORAGE_SLOT} from "./interfaces/ISafe.sol";
 import {ITransactionGuard} from "./interfaces/ITransactionGuard.sol";
 
 contract SafeGuard is IModuleGuard, ITransactionGuard {
@@ -21,29 +21,62 @@ contract SafeGuard is IModuleGuard, ITransactionGuard {
     struct Configuration {
         Authorization[] delegates;
         Authorization[] modules;
+        Authorization[] transferTargets;
     }
 
+    // ERC20 function selectors
+    bytes4 private constant APPROVE_SELECTOR =
+        bytes4(keccak256("approve(address,uint256)"));
+    bytes4 private constant TRANSFER_SELECTOR =
+        bytes4(keccak256("transfer(address,uint256)"));
+    bytes4 private constant MULTISEND_SELECTOR =
+        bytes4(keccak256("multiSend(bytes)"));
+
     uint256 public constant TIMELOCK = 7 days;
-    bytes32 public constant DISABLE = bytes32(uint256(keccak256("disable")) - 1);
+    bytes32 public constant DISABLE =
+        bytes32(uint256(keccak256("disable")) - 1);
 
     mapping(address safe => Account account) public accounts;
-    mapping(address delegate => mapping(address safe => bool authorized)) public delegates;
-    mapping(address module => mapping(address safe => bool authorized)) public modules;
+    mapping(address delegate => mapping(address safe => bool authorized))
+        public delegates;
+    mapping(address module => mapping(address safe => bool authorized))
+        public modules;
+    mapping(address transferTarget => mapping(address safe => bool authorized))
+        public transferTargets;
 
     event Initialized(address indexed safe);
     event Configured(address indexed safe, bytes32 configurationHash);
-    event DelegateAuthorization(address indexed safe, address indexed delegate, bool authorized);
-    event ModuleAuthorization(address indexed safe, address indexed module, bool authorized);
-    event ConfigurationScheduled(address indexed safe, bytes32 configurationHash, uint256 timelock);
+    event DelegateAuthorization(
+        address indexed safe,
+        address indexed delegate,
+        bool authorized
+    );
+    event ModuleAuthorization(
+        address indexed safe,
+        address indexed module,
+        bool authorized
+    );
+    event TransferAuthorization(
+        address indexed safe,
+        address indexed transferTarget,
+        bool authorized
+    );
+    event ConfigurationScheduled(
+        address indexed safe,
+        bytes32 configurationHash,
+        uint256 timelock
+    );
     event DisableScheduled(address indexed safe, uint256 timelock);
 
     error NotEnabled(address safe);
     error AlreadyInitialized(address safe);
     error NotInitialized(address safe);
     error UnauthorizedDelegateCall(address safe, address to);
+    error UnauthorizedTransferCall(address safe, address to);
     error UnauthorizedModule(address safe, address module);
     error UnauthorizedConfiguration(address safe, bytes32 configurationHash);
     error UnauthorizedDisable(address safe);
+    error InvalidMultisendData();
 
     function initialize(Configuration calldata configuration) external {
         if (!_isEnabled(msg.sender)) {
@@ -67,7 +100,10 @@ contract SafeGuard is IModuleGuard, ITransactionGuard {
         bytes32 configurationHash = keccak256(abi.encode(configuration));
 
         Account storage account = accounts[msg.sender];
-        if (account.configurationHash != configurationHash || account.timelock > block.timestamp) {
+        if (
+            account.configurationHash != configurationHash ||
+            account.timelock > block.timestamp
+        ) {
             revert UnauthorizedConfiguration(msg.sender, configurationHash);
         }
 
@@ -104,17 +140,23 @@ contract SafeGuard is IModuleGuard, ITransactionGuard {
         emit DisableScheduled(msg.sender, timelock);
     }
 
-    function supportsInterface(bytes4 interfaceId) external pure returns (bool supported) {
-        supported = interfaceId == type(IERC165).interfaceId || interfaceId == type(IModuleGuard).interfaceId
-            || interfaceId == type(ITransactionGuard).interfaceId;
+    function supportsInterface(
+        bytes4 interfaceId
+    ) external pure returns (bool supported) {
+        supported =
+            interfaceId == type(IERC165).interfaceId ||
+            interfaceId == type(IModuleGuard).interfaceId ||
+            interfaceId == type(ITransactionGuard).interfaceId;
     }
 
-    function checkModuleTransaction(address to, uint256, bytes calldata, uint8 operation, address module)
-        external
-        view
-        returns (bytes32)
-    {
-        _checkTransaction(msg.sender, to, operation);
+    function checkModuleTransaction(
+        address to,
+        uint256 value,
+        bytes calldata data,
+        uint8 operation,
+        address module
+    ) external returns (bytes32) {
+        _checkTransaction(msg.sender, to, data, value, operation);
         if (!modules[msg.sender][module]) {
             revert UnauthorizedModule(msg.sender, module);
         }
@@ -127,8 +169,8 @@ contract SafeGuard is IModuleGuard, ITransactionGuard {
 
     function checkTransaction(
         address to,
-        uint256,
-        bytes calldata,
+        uint256 value,
+        bytes calldata data,
         uint8 operation,
         uint256,
         uint256,
@@ -137,44 +179,105 @@ contract SafeGuard is IModuleGuard, ITransactionGuard {
         address payable,
         bytes calldata,
         address
-    ) external view {
-        _checkTransaction(msg.sender, to, operation);
+    ) external {
+        _checkTransaction(msg.sender, to, data, value, operation);
     }
 
     function checkAfterExecution(bytes32, bool) external {
         _checkAfterExecution(msg.sender);
     }
 
-    function _applyConfiguration(Configuration calldata configuration) internal {
+    function _applyConfiguration(
+        Configuration calldata configuration
+    ) internal {
         for (uint256 i = 0; i < configuration.delegates.length; i++) {
             Authorization calldata delegate = configuration.delegates[i];
             delegates[msg.sender][delegate.addr] = delegate.authorized;
-            emit DelegateAuthorization(msg.sender, delegate.addr, delegate.authorized);
+            emit DelegateAuthorization(
+                msg.sender,
+                delegate.addr,
+                delegate.authorized
+            );
         }
         for (uint256 i = 0; i < configuration.modules.length; i++) {
             Authorization calldata module = configuration.modules[i];
             modules[msg.sender][module.addr] = module.authorized;
-            emit ModuleAuthorization(msg.sender, module.addr, module.authorized);
+            emit ModuleAuthorization(
+                msg.sender,
+                module.addr,
+                module.authorized
+            );
+        }
+
+        for (uint256 i = 0; i < configuration.transferTargets.length; i++) {
+            Authorization calldata transferTarget = configuration
+                .transferTargets[i];
+            transferTargets[msg.sender][transferTarget.addr] = transferTarget
+                .authorized;
+            emit TransferAuthorization(
+                msg.sender,
+                transferTarget.addr,
+                transferTarget.authorized
+            );
         }
     }
 
     function _isEnabled(address safe) internal view returns (bool enabled) {
-        address moduleGuard = abi.decode(ISafe(safe).getStorageAt(MODULE_GUARD_STORAGE_SLOT, 1), (address));
-        address transactionGuard = abi.decode(ISafe(safe).getStorageAt(TRANSACTION_GUARD_STORAGE_SLOT, 1), (address));
-        enabled = moduleGuard == address(this) && transactionGuard == address(this);
+        address transactionGuard = abi.decode(
+            ISafe(safe).getStorageAt(TRANSACTION_GUARD_STORAGE_SLOT, 1),
+            (address)
+        );
+        enabled = transactionGuard == address(this);
     }
 
-    function _checkTransaction(address safe, address to, uint8 operation) internal view {
+    function _checkTransaction(
+        address safe,
+        address to,
+        bytes calldata data,
+        uint256 value,
+        uint8 operation
+    ) internal {
         Account storage account = accounts[safe];
-        if (account.initialized && operation == 1 && !delegates[safe][to]) {
-            revert UnauthorizedDelegateCall(safe, to);
+        if (account.initialized) {
+            // Check delegate calls
+            if (operation == 1 && !delegates[safe][to]) {
+                revert UnauthorizedDelegateCall(safe, to);
+            }
+
+            // Check transfer function
+            if (_isApproveOrTransfer(data)) {
+                (
+                    address erc20To,
+                    uint256 erc20Value
+                ) = _decodeApproveOrTransfer(data);
+                if (erc20Value > 0 && !transferTargets[safe][erc20To]) {
+                    revert UnauthorizedTransferCall(safe, erc20To);
+                }
+            }
+
+            if (value > 0 && !transferTargets[safe][to]) {
+                revert UnauthorizedTransferCall(safe, to);
+            }
+
+            // decode multiSend and call _checkTransaction recursively
+            if (operation == 1 && _isMultiSend(data)) {
+                // decode data
+                bytes memory transactions = abi.decode(
+                    data[4:data.length],
+                    (bytes)
+                );
+                this._checkMultiSend(safe, transactions);
+            }
         }
     }
 
     function _checkAfterExecution(address safe) internal {
         if (!_isEnabled(safe)) {
             Account storage account = accounts[safe];
-            if (account.initialized && account.configurationHash != DISABLE || account.timelock > block.timestamp) {
+            if (
+                (account.initialized && account.configurationHash != DISABLE) ||
+                account.timelock > block.timestamp
+            ) {
                 revert UnauthorizedDisable(safe);
             }
 
@@ -182,5 +285,70 @@ contract SafeGuard is IModuleGuard, ITransactionGuard {
             account.configurationHash = bytes32(0);
             account.timelock = 0;
         }
+    }
+
+    function _isApproveOrTransfer(
+        bytes calldata data
+    ) internal pure returns (bool) {
+        if (data.length < 4) {
+            return false; // Not enough data to contain a function selector
+        }
+
+        bytes4 selector = bytes4(data[0:4]);
+        return selector == APPROVE_SELECTOR || selector == TRANSFER_SELECTOR;
+    }
+
+    function _isMultiSend(bytes calldata data) internal pure returns (bool) {
+        if (data.length < 4) {
+            return false; // Not enough data to contain a function selector
+        }
+        bytes4 selector = bytes4(data[0:4]);
+        return selector == MULTISEND_SELECTOR;
+    }
+
+    function _checkMultiSend(address safe, bytes calldata transactions) public {
+        // Iterate over transactions
+        uint256 length = transactions.length;
+        uint256 i = 0;
+
+        while (i < length) {
+            if (length < i + 85) {
+                revert InvalidMultisendData();
+            }
+            uint8 operation = uint8(bytes1(transactions[i:(i + 1)]));
+
+            address to = address(bytes20(transactions[(i + 1):(i + 21)]));
+            uint256 value = uint256(bytes32(transactions[(i + 21):(i + 53)]));
+            uint256 dataLength = uint256(
+                bytes32(transactions[(i + 53):(i + 85)])
+            );
+            bytes calldata data = transactions[(i + 85):(i + 85 + dataLength)];
+
+            _checkTransaction(safe, to, data, value, operation);
+
+            i = i + 85 + dataLength;
+        }
+    }
+
+    function _decodeApproveOrTransfer(
+        bytes calldata data
+    ) internal pure returns (address, uint256) {
+        require(data.length == 4 + 32 + 32, "Invalid data length");
+
+        bytes4 selector;
+        address recipient;
+        uint256 amount;
+
+        assembly {
+            selector := calldataload(data.offset)
+            recipient := calldataload(add(data.offset, 4))
+            amount := calldataload(add(data.offset, 36))
+        }
+
+        require(
+            selector == APPROVE_SELECTOR || selector == TRANSFER_SELECTOR,
+            "Invalid function selector"
+        );
+        return (recipient, amount);
     }
 }
--- test/SafeGuard.t.sol
+++ test/SafeGuard.t.sol
@@ -2,14 +2,386 @@
 pragma solidity ^0.8.13;
 
 import {Test, console} from "forge-std/Test.sol";
+
 import {SafeGuard} from "../src/SafeGuard.sol";
+import {ISafe} from "../src/interfaces/ISafe.sol";
+
+contract MockStorage is ISafe {
+    uint256 constant TRANSACTION_GUARD_STORAGE_SLOT =
+        0x4a204f620c8c5ccdca3fd54d003badd85ba500436a431f0cbda4f558c93c34c8;
+    function getStorageAt(
+        uint256 offset,
+        uint256 length
+    ) external view returns (bytes memory) {
+        if (offset == TRANSACTION_GUARD_STORAGE_SLOT) {
+            return abi.encode(msg.sender);
+        }
+
+        return abi.encode(address(0));
+    }
+}
 
 contract SafeGuardTest is Test {
+    uint256 constant TRANSACTION_GUARD_STORAGE_SLOT =
+        0x4a204f620c8c5ccdca3fd54d003badd85ba500436a431f0cbda4f558c93c34c8;
+
     SafeGuard public guard;
+
+    ISafe public safe;
+
+    address token = vm.addr(1);
+    address multiSendAddr = vm.addr(2);
 
     function setUp() public {
         guard = new SafeGuard();
+        safe = new MockStorage();
+
+        // Overwrite storage slot to "enable" guard
+        bytes32 slot = bytes32(TRANSACTION_GUARD_STORAGE_SLOT);
+        vm.store(
+            address(safe),
+            slot,
+            bytes32(bytes20(uint160(address(guard))))
+        );
     }
 
-    function test_Nothing() public {}
+    function test_native_transfer() public {
+        SafeGuard.Authorization[]
+            memory delegates = new SafeGuard.Authorization[](1);
+        delegates[0] = SafeGuard.Authorization({
+            addr: multiSendAddr,
+            authorized: true
+        });
+
+        SafeGuard.Authorization[]
+            memory modules = new SafeGuard.Authorization[](0);
+
+        SafeGuard.Authorization[]
+            memory transferTargets = new SafeGuard.Authorization[](1);
+        transferTargets[0] = SafeGuard.Authorization({
+            addr: address(0x123),
+            authorized: true
+        });
+
+        // Construct the Configuration struct
+        SafeGuard.Configuration memory config = SafeGuard.Configuration({
+            delegates: delegates,
+            modules: modules,
+            transferTargets: transferTargets
+        });
+
+        // Call initialize function on target contract
+        vm.prank(address(safe));
+        guard.initialize(config);
+
+        // Benign tx ERC20
+        vm.prank(address(safe));
+        guard.checkTransaction(
+            address(0x123),
+            69,
+            "",
+            0,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+
+        // malicious tx
+        vm.prank(address(safe));
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                SafeGuard.UnauthorizedTransferCall.selector,
+                address(safe),
+                address(0xbad)
+            )
+        );
+        guard.checkTransaction(
+            address(0xbad),
+            420,
+            "",
+            0,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+    }
+
+    function test_erc20_transfer() public {
+        SafeGuard.Authorization[]
+            memory delegates = new SafeGuard.Authorization[](0);
+
+        SafeGuard.Authorization[]
+            memory modules = new SafeGuard.Authorization[](0);
+
+        SafeGuard.Authorization[]
+            memory transferTargets = new SafeGuard.Authorization[](1);
+        transferTargets[0] = SafeGuard.Authorization({
+            addr: address(0x123),
+            authorized: true
+        });
+
+        // Construct the Configuration struct
+        SafeGuard.Configuration memory config = SafeGuard.Configuration({
+            delegates: delegates,
+            modules: modules,
+            transferTargets: transferTargets
+        });
+
+        // Call initialize function on target contract
+        vm.prank(address(safe));
+        guard.initialize(config);
+
+        // Try to execute a transfer to the target address
+        bytes memory goodData = abi.encodeWithSelector(
+            bytes4(keccak256("transfer(address,uint256)")),
+            address(0x123),
+            uint256(200)
+        );
+
+        bytes memory badData = abi.encodeWithSelector(
+            bytes4(keccak256("transfer(address,uint256)")),
+            address(0xbad),
+            uint256(200)
+        );
+        // Benign tx ERC20
+        vm.prank(address(safe));
+        guard.checkTransaction(
+            token,
+            0,
+            goodData,
+            0,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+
+        // malicious tx
+        vm.prank(address(safe));
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                SafeGuard.UnauthorizedTransferCall.selector,
+                address(safe),
+                address(0xbad)
+            )
+        );
+        guard.checkTransaction(
+            token,
+            0,
+            badData,
+            0,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+    }
+
+    function test_erc20_approve() public {
+        SafeGuard.Authorization[]
+            memory delegates = new SafeGuard.Authorization[](0);
+
+        SafeGuard.Authorization[]
+            memory modules = new SafeGuard.Authorization[](0);
+
+        SafeGuard.Authorization[]
+            memory transferTargets = new SafeGuard.Authorization[](1);
+        transferTargets[0] = SafeGuard.Authorization({
+            addr: address(0x123),
+            authorized: true
+        });
+
+        // Construct the Configuration struct
+        SafeGuard.Configuration memory config = SafeGuard.Configuration({
+            delegates: delegates,
+            modules: modules,
+            transferTargets: transferTargets
+        });
+
+        // Call initialize function on target contract
+        vm.prank(address(safe));
+        guard.initialize(config);
+
+        // Try to execute a transfer to the target address
+        bytes memory goodData = abi.encodeWithSelector(
+            bytes4(keccak256("approve(address,uint256)")),
+            address(0x123),
+            uint256(200)
+        );
+
+        // Try to execute a transfer to the target address
+        bytes memory revokeData = abi.encodeWithSelector(
+            bytes4(keccak256("approve(address,uint256)")),
+            address(0xbad),
+            uint256(0)
+        );
+
+        bytes memory badData = abi.encodeWithSelector(
+            bytes4(keccak256("approve(address,uint256)")),
+            address(0xbad),
+            uint256(200)
+        );
+        // Benign approval
+        vm.prank(address(safe));
+        guard.checkTransaction(
+            token,
+            0,
+            goodData,
+            0,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+
+        // Revoking any approval is possible
+        vm.prank(address(safe));
+        guard.checkTransaction(
+            token,
+            0,
+            revokeData,
+            0,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+
+        // malicious approval
+        vm.prank(address(safe));
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                SafeGuard.UnauthorizedTransferCall.selector,
+                address(safe),
+                address(0xbad)
+            )
+        );
+        guard.checkTransaction(
+            token,
+            0,
+            badData,
+            0,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+    }
+
+    function testMultiSend() public {
+        SafeGuard.Authorization[]
+            memory delegates = new SafeGuard.Authorization[](1);
+
+        delegates[0] = SafeGuard.Authorization({
+            addr: multiSendAddr,
+            authorized: true
+        });
+
+        SafeGuard.Authorization[]
+            memory modules = new SafeGuard.Authorization[](0);
+
+        SafeGuard.Authorization[]
+            memory transferTargets = new SafeGuard.Authorization[](1);
+        transferTargets[0] = SafeGuard.Authorization({
+            addr: address(0x123),
+            authorized: true
+        });
+
+        // Construct the Configuration struct
+        SafeGuard.Configuration memory config = SafeGuard.Configuration({
+            delegates: delegates,
+            modules: modules,
+            transferTargets: transferTargets
+        });
+
+        // Call initialize function on target contract
+        vm.prank(address(safe));
+        guard.initialize(config);
+
+        // Multisend with two native transfers to 0x00..00123
+        bytes
+            memory nativeTransfers = hex"8d80ff0a000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000aa0000000000000000000000000000000000000001230000000000000000000000000000000000000000000000001bc16d674ec8000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000001230000000000000000000000000000000000000000000000000de0b6b3a7640000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000";
+
+        bytes
+            memory erc20Transfers = hex"8d80ff0a00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000132005afe3855358e112b5647b952709e6165e1c1eeee00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044a9059cbb000000000000000000000000000000000000000000000000000000000000012300000000000000000000000000000000000000000000000000000000000001a4005afe3855358e112b5647b952709e6165e1c1eeee00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044a9059cbb000000000000000000000000000000000000000000000000000000000000012300000000000000000000000000000000000000000000000000000000000000450000000000000000000000000000";
+
+        // Contains one transfer to a non allow listed address
+        bytes
+            memory maliciousTransfer = hex"8d80ff0a00000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000132005afe3855358e112b5647b952709e6165e1c1eeee00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044a9059cbb0000000000000000000000000000000000000000000000000000000000000bad00000000000000000000000000000000000000000000000000000000000001a4005afe3855358e112b5647b952709e6165e1c1eeee00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000044a9059cbb000000000000000000000000000000000000000000000000000000000000012300000000000000000000000000000000000000000000000000000000000000450000000000000000000000000000";
+
+        vm.prank(address(safe));
+        guard.checkTransaction(
+            multiSendAddr,
+            0,
+            nativeTransfers,
+            1,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+
+        vm.prank(address(safe));
+        guard.checkTransaction(
+            multiSendAddr,
+            0,
+            erc20Transfers,
+            1,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+
+        vm.prank(address(safe));
+        vm.expectRevert(
+            abi.encodeWithSelector(
+                SafeGuard.UnauthorizedTransferCall.selector,
+                address(safe),
+                address(0xbad)
+            )
+        );
+        guard.checkTransaction(
+            multiSendAddr,
+            0,
+            maliciousTransfer,
+            1,
+            uint256(0),
+            uint256(0),
+            uint256(0),
+            address(0),
+            payable(address(0)),
+            abi.encode(uint8(0)),
+            address(0)
+        );
+    }
 }
